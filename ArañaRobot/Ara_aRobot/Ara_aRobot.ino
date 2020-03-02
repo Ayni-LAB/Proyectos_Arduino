739ffbef701f3d5fd43e59766c66a611885e1606
Ayni-LAB <ivan.arenas@ayni-innovation.pe>
2020-03-02 12:24:30 -0500

    Comentarios Araña

--- ArañaRobot/Ara_aRobot/Ara_aRobot.ino
+++ ArañaRobot/Ara_aRobot/Ara_aRobot.ino
@@ -5,6 +5,8 @@ Fecha= 10/02/2020
 Autor= Brigitte Oviedo
 */
 #include <Servo.h> //Incluye la librerias necesarias para el servo
+
+//Nombrando los servos a trabajar
 Servo   Mano1;
 Servo   Base1;
 Servo   Codo1;
@@ -48,46 +50,44 @@ void setup()
    //Se estabiliza la araña
    condicion_inicial();
    delay(2000);
-
-  char estado;
  
  }
 void loop(){
-      if(Serial.available()>0){ // Lee el bluetooth y almacena en estado
-         estado = Serial.read(); //Le lo mandado por el Celular
+    if(Serial.available()>0){ // Lee el bluetooth y almacena en estado
+        estado = Serial.read(); //Le lo mandado por el Celular
 
-         if(estado=='f'){     //Camina hacia adelante   
-           for (int i=0;i<2;i++){
-            Adelante();
-           }
-           condicion_inicial();
-         }
-         if(estado=='t'){     //Camina hacia atras  
-           for (int i=0;i<2;i++){
-            Atras();
-           }
-           condicion_inicial();
-         }
-         if(estado=='d'){     //Gira hacia la derecha 
-            for (int i=0;i<2;i++){
-            Derecha();
-           }
-         }
-         if(estado=='i'){     //Gira hacia la izquierda
+        if(estado=='f'){     //Camina hacia adelante   
           for (int i=0;i<2;i++){
-            Izquierda();
-           }
-         }
-         if(estado=='x'){     //Sube y baja 3 veces
-            for (int i=0;i<=2;i++){
-              subir_bajar();  
-            }
-         }
-         if(estado=='o'){     //Sube y baja 3 veces
-              condicion_inicial();  
-         }
-      
-      }
+          Adelante();
+          }
+          condicion_inicial();
+        }
+        if(estado=='t'){     //Camina hacia atras  
+          for (int i=0;i<2;i++){
+          Atras();
+          }
+          condicion_inicial();
+        }
+        if(estado=='d'){     //Gira hacia la derecha 
+          for (int i=0;i<2;i++){
+          Derecha();
+          }
+        }
+        if(estado=='i'){     //Gira hacia la izquierda
+        for (int i=0;i<2;i++){
+          Izquierda();
+          }
+        }
+        if(estado=='x'){     //Sube y baja 3 veces
+          for (int i=0;i<=2;i++){
+            subir_bajar();  
+          }
+        }
+        if(estado=='o'){     //Sube y baja 3 veces
+            condicion_inicial();  
+        }
+    
+    }
 }
 
 //Secuencia para que suba y baje la araña
@@ -97,7 +97,6 @@ void subir_bajar(){
   bajar();
   delay(600);
   condicion_inicial();
-
 }
 
 //Condicion inicial de los servos de la araña
@@ -122,221 +121,220 @@ void condicion_inicial()
 
 //Funcion para que baje
 void bajar(){         
-    Codo1.write(130);
-    Mano1.write(130);
-    Base1.write(90); 
-//    
-    Codo2.write(50);
-    Mano2.write(50);
-    Base2.write(90);
-//    
-    Codo3.write(130);
-    Mano3.write(130);
-    Base3.write(90);
-//    
-    Codo4.write(50);
-    Mano4.write(50);
-    Base4.write(90);
+  Codo1.write(130);
+  Mano1.write(130);
+  Base1.write(90); 
+  
+  Codo2.write(50);
+  Mano2.write(50);
+  Base2.write(90);
+
+  Codo3.write(130);
+  Mano3.write(130);
+  Base3.write(90);
+  
+  Codo4.write(50);
+  Mano4.write(50);
+  Base4.write(90);
 }
 
 //Funcion caminar hacia adelante
 void Adelante()
-{
-  
-BaseAdelante();
-MoverPata4f();
-MoverPata1f();
+{  
+  BaseAdelante();
+  MoverPata4f();
+  MoverPata1f();
 
-MoverPata3f();
-MoverPata2f();
-BaseAdelante();
+  MoverPata3f();
+  MoverPata2f();
+  BaseAdelante();
 }
 
 //Funcion caminar hacia atras
 void Atras()
 {
-BaseAdelante();
-MoverPata2();
-MoverPata3();
+  BaseAdelante();
+  MoverPata2();
+  MoverPata3();
 
-MoverPata1();
-MoverPata4();
-BaseAdelante();
+  MoverPata1();
+  MoverPata4();
+  BaseAdelante();
 }
 
 //Funcion girar hacia la derecha
-void Derecha(){
-    
-    Codo3.write(120);
-    Base3.write(130);
-    delay(200);
-    Codo3.write(90);
-    Mano3.write(90);
-    delay(tiempo); 
+void Derecha()
+{    
+  Codo3.write(120);
+  Base3.write(130);
+  delay(200);
+  Codo3.write(90);
+  Mano3.write(90);
+  delay(tiempo); 
 
-    Codo2.write(60);
-    Base2.write(130);
-    delay(200);
-    Codo2.write(90);
-    Mano2.write(90);
-    delay(tiempo);
+  Codo2.write(60);
+  Base2.write(130);
+  delay(200);
+  Codo2.write(90);
+  Mano2.write(90);
+  delay(tiempo);
 
-    Codo1.write(120);
-    Base1.write(130);
-    delay(200);
-    Codo1.write(90);
-    Mano1.write(90);
-    delay(tiempo);
-    
-    Codo4.write(60);
-    Base4.write(130);
-    delay(200);
-    Codo4.write(90);
-    Mano4.write(90);
-    delay(tiempo);
-    
-    Base3.write(90);
-    Base2.write(90);
-    Base1.write(90);
-    Base4.write(90);
-    delay(200); 
-  }
+  Codo1.write(120);
+  Base1.write(130);
+  delay(200);
+  Codo1.write(90);
+  Mano1.write(90);
+  delay(tiempo);
+  
+  Codo4.write(60);
+  Base4.write(130);
+  delay(200);
+  Codo4.write(90);
+  Mano4.write(90);
+  delay(tiempo);
+  
+  Base3.write(90);
+  Base2.write(90);
+  Base1.write(90);
+  Base4.write(90);
+  delay(200); 
+}
 
 //Funcion girar hacia la derecha
-void Izquierda(){
-    Codo4.write(60);
-    Base4.write(50);
-    delay(200);
-    Codo4.write(90);
-    Mano4.write(90);
-    delay(tiempo); 
+void Izquierda()
+{
+  Codo4.write(60);
+  Base4.write(50);
+  delay(200);
+  Codo4.write(90);
+  Mano4.write(90);
+  delay(tiempo); 
 
-    Codo1.write(120);
-    Base1.write(50);
-    delay(200);
-    Codo1.write(90);
-    Mano1.write(90);
-    delay(tiempo); 
-        
-    Codo2.write(60);
-    Base2.write(50);
-    delay(200);
-    Codo2.write(90);
-    Mano2.write(90);
-    delay(tiempo);
+  Codo1.write(120);
+  Base1.write(50);
+  delay(200);
+  Codo1.write(90);
+  Mano1.write(90);
+  delay(tiempo); 
+      
+  Codo2.write(60);
+  Base2.write(50);
+  delay(200);
+  Codo2.write(90);
+  Mano2.write(90);
+  delay(tiempo);
 
-    Codo3.write(120);
-    Base3.write(50);
-    delay(200);
-    Codo3.write(90);
-    Mano3.write(90);
-    delay(tiempo);
- 
-    Base3.write(90);
-    Base2.write(90);
-    Base1.write(90);
-    Base4.write(90);
-    delay(200);
-  }
+  Codo3.write(120);
+  Base3.write(50);
+  delay(200);
+  Codo3.write(90);
+  Mano3.write(90);
+  delay(tiempo);
+
+  Base3.write(90);
+  Base2.write(90);
+  Base1.write(90);
+  Base4.write(90);
+  delay(200);
+}
 
 //////////////////////////////////////////////////
 //Sub-Funciones para Caminar hacia adelante
 //////////////////////////////////////////////////
 void BaseAdelante()
-{
-    Base1.write(110); //70
-    Base2.write(70);  //90
-    Base3.write(90);  //90
-    Base4.write(90); //120
-    delay(tiempo);   
+{ 
+  Base1.write(110); //70
+  Base2.write(70);  //90
+  Base3.write(90);  //90
+  Base4.write(90); //120
+  delay(tiempo);   
 }
 
 void MoverPata4f()
 {
-    Codo4.write(60);
-    Base4.write(60);
-    delay(200);
-    Codo4.write(90);
-    Mano4.write(90);
-   
-    delay(tiempo);   
+  Codo4.write(60);
+  Base4.write(60);
+  delay(200);
+  Codo4.write(90);
+  Mano4.write(90);
+  
+  delay(tiempo);   
 }
 void MoverPata1f()
 {
-    Codo1.write(120);
-    Base1.write(50);
-    delay(200);
-    Codo1.write(90);
-    Mano1.write(90);
-   
-    delay(tiempo);   
+  Codo1.write(120);
+  Base1.write(50);
+  delay(200);
+  Codo1.write(90);
+  Mano1.write(90);
+  
+  delay(tiempo);     
 }
 void MoverPata3f()
 {
-    Codo3.write(120);
-    Base3.write(130);
-    delay(200);
-    Codo3.write(90);
-    Mano3.write(90);
- 
-    delay(tiempo);   
+  Codo3.write(120);
+  Base3.write(130);
+  delay(200);
+  Codo3.write(90);
+  Mano3.write(90);
+
+  delay(tiempo);   
 }
 void MoverPata2f()
 {
-    Codo2.write(60);
-    Base2.write(120);
-    delay(200);
-    Codo2.write(90);
-    Mano2.write(90);
-   
-    delay(tiempo);   
+  Codo2.write(60);
+  Base2.write(120);
+  delay(200);
+  Codo2.write(90);
+  Mano2.write(90);
+  
+  delay(tiempo);   
 }
 
 //////////////////////////////////
 //Sub-Funciones para Caminar hacia atras
 //////////////////////////////////
 void BaseAtras(){
-    Base3.write(50); //70
-    Base4.write(130);  //90
-    Base1.write(90);  //90
-    Base2.write(90); //120
-    delay(tiempo);      
+  Base3.write(50); //70
+  Base4.write(130);  //90
+  Base1.write(90);  //90
+  Base2.write(90); //120
+  delay(tiempo);      
 }
-
 void MoverPata2()
 {
-    Codo2.write(60);
-    Base2.write(40);
-    delay(200);
-    Codo2.write(90);
-    Mano2.write(90);
-    delay(tiempo);   
+  Codo2.write(60);
+  Base2.write(40);
+  delay(200);
+  Codo2.write(90);
+  Mano2.write(90);
+  delay(tiempo);   
 }
 void MoverPata3()
 {
-    Codo3.write(120);
-    Base3.write(50);
-    delay(200);
-    Codo3.write(90);
-    Mano3.write(90);
-    delay(tiempo);   
+  Codo3.write(120);
+  Base3.write(50);
+  delay(200);
+  Codo3.write(90);
+  Mano3.write(90);
+  delay(tiempo);   
 }
 void MoverPata1()
 {
-    Codo1.write(120);
-    Base1.write(130);
-    delay(200);
-    Codo1.write(90);
-    Mano1.write(90);
-    delay(tiempo);   
+  Codo1.write(120);
+  Base1.write(130);
+  delay(200);
+  Codo1.write(90);
+  Mano1.write(90);
+  delay(tiempo);   
 }
 void MoverPata4()
 {
-    Codo4.write(60);
-    Base4.write(120);
-    delay(200);
-    Codo4.write(90);
-    Mano4.write(90);
-    delay(tiempo);   
+  Codo4.write(60);
+  Base4.write(120);
+  delay(200);
+  Codo4.write(90);
+  Mano4.write(90);
+  delay(tiempo);   
 }
 
